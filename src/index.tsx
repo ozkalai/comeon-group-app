bef3e7e403559f06adfe99ce21fd0390bc172676
ozkalai <melihozkalayy@gmail.com>
2022-10-14 15:31:35 +0300

    feat: browser router

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { MantineProvider } from "@mantine/core";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <MantineProvider>
-      <App />
-    </MantineProvider>
+    <BrowserRouter>
+      <MantineProvider>
+        <App />
+      </MantineProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
