f630e15ae986a62ab9e6a07cc4b85ca8100e1e42
ozkalai <melihozkalayy@gmail.com>
2022-10-14 15:31:14 +0300

    refactor: removed unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
 import Layout from "./components/Layout";
 import Login from "./pages/login";
 import { AuthProvider } from "./lib/hooks/useAuth";
+import RequireAuth from "./lib/helper/requiredAuth";
+import Casino from "./pages/casino";
 
 function App() {
   return (
     <div className="App">
       <AuthProvider>
-        <Layout>
-          <Login />
-        </Layout>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/"
+              element={
+                <RequireAuth>
+                  <Casino />
+                </RequireAuth>
+              }
+            />
+          </Route>
+        </Routes>
       </AuthProvider>
     </div>
   );
