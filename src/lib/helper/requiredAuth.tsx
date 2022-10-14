ecfda2bed33f1359be5772664df17fb680129af5
ozkalai <melihozkalayy@gmail.com>
2022-10-14 22:29:45 +0300

    fix: missing import

--- src/lib/helper/requiredAuth.tsx
+++ src/lib/helper/requiredAuth.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Navigate, useLocation } from "react-router";
 import useAuth from "../hooks/useAuth";
 
@@ -6,10 +7,6 @@ function RequireAuth({ children }: { children: JSX.Element }) {
   let location = useLocation();
 
   if (!auth.user) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to when they were redirected. This allows us to send them
-    // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
