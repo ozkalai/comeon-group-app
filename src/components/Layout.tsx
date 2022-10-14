f5223421366714ac5ba8c349f189c4de2cc2b9a9
ozkalai <melihozkalayy@gmail.com>
2022-10-14 22:29:21 +0300

    style: fix padding

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <div className="bg-[#444] min-h-screen p-5">
+    <div className="bg-[#444] min-h-screen p-5 pb-12">
       <img
         src="images/logo.svg"
         className="logo  w-4/5 sm:w-2/5 mx-auto"
